7cc459d1148023ddb3f3eadaeb5ba1964a0e86cc
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-18 13:23:47 +0100

    Ensure that an empty object is returned if an empty array is passed as an argument in the function

--- src/test-without-mocha.js
+++ src/test-without-mocha.js
@@ -1,8 +1,17 @@
-const ShoppingCart = require("./ShoppingCart")
-const cart = new ShoppingCart()
-cart.getItems()
-cart.addItem("Trash can", 1, 15.5)
-cart.getItems()
-cart.clear()
-cart.getItems()
-cart.total()
+// const ShoppingCart = require("./ShoppingCart")
+// const cart = new ShoppingCart()
+// cart.getItems()
+// cart.addItem("Trash can", 1, 15.5)
+// cart.getItems()
+// cart.clear()
+// cart.getItems()
+// cart.total()
+
+// const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
+// const grouping = groupAdultsByAgeRange([])
+// console.log(grouping)
+// const grouping2 = groupAdultsByAgeRange([
+//   { name: "pete", age: 10 },
+//   { name: "dove", age: 17 },
+// ])
+// console.log(grouping2)
--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,10 @@
-module.exports.groupAdultsByAgeRange = function() {
-
+module.exports.groupAdultsByAgeRange = function(objectArray) {
+  const ifEmpty = {}
+  
+  if(objectArray[0] === undefined) {
+    return ifEmpty
+  }
+  else {
+    return objectArray
+  }
 }
