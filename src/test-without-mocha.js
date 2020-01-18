2c000ff454542f6ff91807f136c4883290fb5524
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-18 15:37:39 +0100

    Ensure that the result only contains non-empty groups and that the result groups people by age in arrays

--- src/test-without-mocha.js
+++ src/test-without-mocha.js
@@ -8,10 +8,25 @@
 // cart.total()
 
 const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
-// const grouping = groupAdultsByAgeRange([])
-// console.log(grouping)
-const grouping2 = groupAdultsByAgeRange([
-  { name: "pete", age: 10 },
-  { name: "dove", age: 17 },
-])
-console.log(grouping2)
+const grouping = groupAdultsByAgeRange([])
+console.log(grouping)
+// const grouping2 = groupAdultsByAgeRange([
+//   { name: "pete", age: 10 },
+//   { name: "dove", age: 17 },
+//   { name: "dave", age: 18 },
+//   { name: "hall", age: 19 },
+//   { name: "donn", age: 20 },
+//   { name: "trey", age: 21 },
+//   { name: "hann", age: 29 },
+//   { name: "chew", age: 30 },
+//   { name: "cloe", age: 31 },
+//   { name: "dart", age: 39 },
+//   { name: "this", age: 40 },
+//   { name: "dame", age: 41 },
+//   { name: "henk", age: 49 },
+//   { name: "anna", age: 50 },
+//   { name: "olga", age: 51 },
+//   { name: "bert", age: 52 },
+//   { name: "oldd", age: 120 },
+// ])
+// console.log(grouping2)
--- src/transform.js
+++ src/transform.js
@@ -1,10 +1,48 @@
 module.exports.groupAdultsByAgeRange = function(objectArray) {
+
   const over18 = objectArray.filter(person => person.age >= 18)
   
-  if(over18[0] === undefined) {
-    return {}
+  const result = {
+    '20 and younger': [],
+    '21-30': [],
+    '31-40': [],
+    '41-50': [],
+    '51 and older': []
   }
-  else {
-    return over18
+
+  const categorize = over18.reduce((people, currentPerson) => {
+    return currentPerson.age <= 20 ? 
+    result['20 and younger'].push(currentPerson) 
+    : currentPerson.age >= 21 && currentPerson.age <= 30 ?
+    result['21-30'].push(currentPerson)  
+    : currentPerson.age >= 31 && currentPerson.age <= 40 ?
+    result['31-40'].push(currentPerson)  
+    : currentPerson.age >= 41 && currentPerson.age <= 50 ?
+    result['41-50'].push(currentPerson) 
+    : result['51 and older'].push(currentPerson)  
+  }, 0)
+
+  if(result['20 and younger'].length == 0) {
+    delete result['20 and younger']
   }
+  if(result['21-30'].length == 0) {
+    delete result['21-30']
+  }
+  if(result['31-40'].length == 0) {
+    delete result['31-40'] 
+  }
+  if(result['41-50'].length == 0) {
+    delete result['41-50'] 
+  }
+  if(result['51 and older'].length == 0) {
+    delete result['51 and older']
+  }
+  return result
+
+  // if(over18[0] === undefined) {
+  //   return result
+  // }
+  // else {
+  //   return over18
+  // }
 }
