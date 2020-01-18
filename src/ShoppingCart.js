329e0df1785563d6a8a3be569bcf382745d7e270
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-18 12:21:19 +0100

    Added getItems() method and addItems() method to the class in ShoppingCart.js and tested them using mocha as well as without mocha using the test-without-mocha.js file

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -3,6 +3,21 @@ module.exports = class {
     this.items = []
   }
 
+  getItems() {
+    // console.log(this.items)
+    return this.items
+  }
+  
+  
+  addItem(itemName, quantity, price) {
+    const item = {
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit: price
+    }
+    this.items.push(item)
+    return this.items
+  }
  
 }
 
--- src/test-without-mocha.js
+++ src/test-without-mocha.js
@@ -1,4 +1,5 @@
 const ShoppingCart = require("./ShoppingCart")
 const cart = new ShoppingCart()
 cart.getItems()
-
+cart.addItem("Trash can", 1, 15.5)
+cart.getItems()
